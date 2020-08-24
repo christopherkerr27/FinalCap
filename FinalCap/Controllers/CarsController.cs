a3c8d96264d4cecd0d836398dfad59f7b05697c1
Christopher Kerr <christopherke@truck-hero.com>
2020-08-24 18:24:23 -0400

    chris

---
+++ FinalCap/Controllers/CarsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalCap.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly ICarsService _service;
+        public CarsController(ICarsService service)
+        {
+            _service = service;
+        }
+        public async Task<ActionResult> Index()
+        {
+            var model = await _service.GetAll();
+            return View(model);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                await _services.Create(car);
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+      
+    }
+}
---
+++ FinalCapApi/Controllers/CarsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalCap.Models;
+using FinalCapApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FinalCapApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    private static List<Car> _cars = new List<Car>();
+
+    [HttpGet]
+    public IEnumerable<Car> Get()
+    {
+        return Car;
+    }
+
+    [HttpPost]
+    public IActionResult Post(Car car)
+    {
+        _cars.Add(car);
+        return Created($"/api/books/{car.CarId}", car);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        var car = _cars.FirstOrDefault(x => x.CarId == id);
+        if (car == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(car);
+    }
+}
